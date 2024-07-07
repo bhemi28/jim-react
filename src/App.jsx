3575aacda5f73fe17b8e4ad3a41b018e18d9dddf
Meet Bhesaniya <meetbhesaniya191@gmail.com>
2024-07-07 15:16:52 +0530

    Basic structure of the app | nothing new

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Hero from './components/Hero'
+import Generator from './components/Generator'
+import Workout from './components/Workout'
+
+function App() {
+ 
+
+  return (
+    <main>
+     <Hero />
+     <Generator />
+     <Workout />
+    </main >
+  )
+}
+
+export default App
