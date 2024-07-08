a23664fa08a71e6748dca2669c320c0444a16d59
bhemi28 <meetbhesaniya191@gmail.com>
2024-07-08 12:56:05 +0530

    add buttons in generator, add remixicon cdn

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
+import { WORKOUTS } from '../utils/swoldier'
+
+const Header = (props) => {
+  const { index, title, description } = props
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <div className='flex items-center justify-center text-center gap-4' >
+        <p className='text-2xl sm:text-3xl  md:text-5xl font-bold text-slate-400'>{index}</p>
+        <h3 className='text-lg sm:text-2xl md:text-3xl font-semibold text-slate-200'>{title}</h3>
+      </div>
+      <p className='text-center text-sm sm:text-base mx-auto'>{description}</p>
+    </div>
+  )
+}
 
 const Generator = () => {
   return (
     <SectionWrapper header={"generate your workout"} title={['It\'s', 'Huge', 'o\'clock']}>
-      adfasdfasa
+      <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to start..."} />
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+
+        {
+          Object.keys(WORKOUTS).map((type, typeIndex) => {
+            return (
+              <button className='bg-slate-950 border border-blue-400 py-2 px-4 rounded-lg duration-200 hover:border-blue-600' key={typeIndex}>
+                <p className='capitalize'>{type.replaceAll('_', " ")}</p>
+              </button>
+            )
+          })
+        }
+      </div>
+
+      <Header index={'02'} title={'Lock on target'} description={"Select the muscles judged for anhilation."} />
+      <div className='bg-slate-950 p-3 border border-blue-400 rounded-lg'>
+        <div className='flex relative items-center justify-center'>
+          <p>Select muscle groups</p>
+          <i class="ri-arrow-down-s-fill absolute top-1/2 -translate-y-1/2 right-3"></i>
+        </div>
+      </div>
     </SectionWrapper>
   )
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -5,10 +5,13 @@ const SectionWrapper = (props) => {
     const {children, header, title} = props
     
   return (
-    <section className='flex flex-col gap-10 ' >
-        <div className='flex flex-col py-10 items-center justify-center bg-slate-900 text-slate-200 gap-4'>
+    <section className='min-h-screen flex flex-col gap-10 p-4' >
+        <div className='flex flex-col py-10 items-center justify-center bg-slate-950 text-slate-200 gap-4'>
             <p className='uppercase text-base   '>{header}</p>
             <h1 className='font-medium text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>{title[0]} <span className='text-blue-400 uppercase font-bold'>{title[1]}</span> {title[2]}</h1>
+        </div>
+        <div className='max-w-[800px] w-full flex flex-col gap-10 mx-auto p-4'>
+        {children}
         </div>
     </section>
   )
