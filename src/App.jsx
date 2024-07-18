d30280a25ab1eb7af16e00a5809a3b986cdecb4a
Meet Bhesaniya <meetbhesaniya191@gmail.com>
2024-07-18 19:11:40 +0530

    uplift all states from generator.jsx to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-
-import Hero from './components/Hero'
-import Generator from './components/Generator'
-import Workout from './components/Workout'
+import Hero from "./components/Hero";
+import Generator from "./components/Generator";
+import Workout from "./components/Workout";
+import { useState } from "react";
 
 function App() {
- 
+  const [workout, setWorkout] = useState(null);
+  const [poison, setPoison] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoal] = useState("strength_power");
 
   return (
-    <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base'>
-     <Hero />
-     <Generator />
-     <Workout />
-    </main >
-  )
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
+      <Hero />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscles}
+        setMuscles={setMuscles}
+        goal={goal}
+        setGoal={setGoal}
+      />
+      <Workout workout={workout} />
+    </main>
+  );
 }
 
-export default App
+export default App;
