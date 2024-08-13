ef68208f033c34dc8376265a4b61e05832006331
Meet Bhesaniya <meetbhesaniya191@gmail.com>
2024-08-13 09:24:12 +0530

    chage in Button.jsx, ExcerciseCard.jsx, Workout.jsx
    Button.jsx -> added a function to the button onclick.
    excerciseCard.jsx -> Created a new ExcerciseCard component.
    Workout.jsx -> uplifting all the states to App.jsx.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Button = (props) => {
 
-    const {text} = props
+    const {text, func} = props
 
   return (
-    <button className="mt-3 mx-auto border-2 group  border-blue-400 rounded text-xl font-semibold px-6 py-3 hover:bg-slate-950 group-hover:text-white text-slate-200 btnShadow shadow-blue-300 duration-200 ">{text}</button>
+    <button onClick={func} className="mt-3 mx-auto border-2 group  border-blue-400 rounded text-xl font-semibold px-6 py-3 hover:bg-slate-950 group-hover:text-white text-slate-200 btnShadow shadow-blue-300 duration-200 ">{text}</button>
   )
 }
 
